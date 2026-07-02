c1e530b03ba567c60ed2fe44a98a8f7bde56c084
Chandrakanth V <chandrakanthvarakala@gmail.com>
2026-07-02 17:25:54 +0530

    Add license and consolidate tool registry

--- src/playwright_python_mcp/backend/tools/__init__.py
+++ src/playwright_python_mcp/backend/tools/__init__.py
@@ -1,17 +1,6 @@
 from __future__ import annotations
 
 from playwright_python_mcp.mcp.config import ServerConfig
-from playwright_python_mcp.tools.registry import (
-    CORE_TOOL_NAMES,
-    CONFIG_TOOL_NAMES,
-    DEVTOOLS_TOOL_NAMES,
-    NETWORK_TOOL_NAMES,
-    PDF_TOOL_NAMES,
-    STORAGE_TOOL_NAMES,
-    TESTING_TOOL_NAMES,
-    TRACING_TOOL_NAMES,
-    VISION_TOOL_NAMES,
-)
 
 from ..tool import Tool
 from .common import common_tools
@@ -39,6 +28,102 @@ from .video import video_tools
 from .verify import verify_tools
 from .webstorage import webstorage_tools
 from .wait import wait_tools
+
+
+CORE_TOOL_NAMES = [
+    "browser_click",
+    "browser_console_messages",
+    "browser_drag",
+    "browser_drop",
+    "browser_evaluate",
+    "browser_file_upload",
+    "browser_fill_form",
+    "browser_handle_dialog",
+    "browser_hover",
+    "browser_select_option",
+    "browser_type",
+    "browser_close",
+    "browser_navigate_back",
+    "browser_navigate",
+    "browser_network_request",
+    "browser_network_requests",
+    "browser_press_key",
+    "browser_resize",
+    "browser_run_code_unsafe",
+    "browser_snapshot",
+    "browser_tabs",
+    "browser_take_screenshot",
+    "browser_wait_for",
+]
+
+PDF_TOOL_NAMES = [
+    "browser_pdf_save",
+]
+
+VISION_TOOL_NAMES = [
+    "browser_mouse_move_xy",
+    "browser_mouse_click_xy",
+    "browser_mouse_drag_xy",
+    "browser_mouse_down",
+    "browser_mouse_up",
+    "browser_mouse_wheel",
+]
+
+TESTING_TOOL_NAMES = [
+    "browser_generate_locator",
+    "browser_verify_element_visible",
+    "browser_verify_text_visible",
+    "browser_verify_list_visible",
+    "browser_verify_value",
+]
+
+NETWORK_TOOL_NAMES = [
+    "browser_network_state_set",
+    "browser_route",
+    "browser_route_list",
+    "browser_unroute",
+]
+
+TRACING_TOOL_NAMES = [
+    "browser_start_tracing",
+    "browser_stop_tracing",
+]
+
+DEVTOOLS_TOOL_NAMES = [
+    "browser_resume",
+    "browser_highlight",
+    "browser_hide_highlight",
+    "browser_annotate",
+    "browser_start_video",
+    "browser_stop_video",
+    "browser_video_chapter",
+    "browser_video_show_actions",
+    "browser_video_hide_actions",
+]
+
+CONFIG_TOOL_NAMES = [
+    "browser_get_config",
+]
+
+STORAGE_TOOL_NAMES = [
+    "browser_cookie_list",
+    "browser_cookie_get",
+    "browser_cookie_set",
+    "browser_cookie_delete",
+    "browser_cookie_clear",
+    "browser_storage_state",
+    "browser_set_storage_state",
+    "browser_localstorage_list",
+    "browser_localstorage_get",
+    "browser_localstorage_set",
+    "browser_localstorage_delete",
+    "browser_localstorage_clear",
+    "browser_sessionstorage_list",
+    "browser_sessionstorage_get",
+    "browser_sessionstorage_set",
+    "browser_sessionstorage_delete",
+    "browser_sessionstorage_clear",
+]
 
 
 IMPLEMENTED_TOOLS = [
